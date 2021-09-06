b55226eeec1aba9d6c386596a10ba8a9fcbc3744
Azeem M <87535744+azeem-mustafa@users.noreply.github.com>
2021-09-06 12:10:43 -0400

    created working/broken header

--- client/src/components/Header/.Header.js
+++ client/src/components/Header/.Header.js
@@ -2,31 +2,31 @@ import React, { Component } from 'react';
 import './_header.scss';
 
 
-Header = () => {
+function Header (){
     
         return (
             <>
             <div>
-            <div class="header-dark">
-                <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
-                    <a class="navbar-brand" href="#">Starter-Code</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                        <div class="collapse navbar-collapse"
+            <div className="header-dark">
+                <nav className="navbar navbar-dark navbar-expand-md navigation-clean-search">
+                    <a className="navbar-brand" href="#">Starter-Code</a><button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                        <div className="collapse navbar-collapse"
                             id="navcol-1">
-                            <ul class="nav navbar-nav">
-                                <li class="nav-item" role="presentation"><a class="nav-link" href="#">Websites</a></li>
-                                <li class="dropdown"><a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">EXP Level</a>
-                                    <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">Beginner</a><a class="dropdown-item" role="presentation" href="#">Intermediate</a><a class="dropdown-item" role="presentation" href="#">Advanced</a></div>
+                            <ul className="nav navbar-nav">
+                                <li className="nav-item" role="presentation"><a className="nav-link" href="#">Websites</a></li>
+                                <li className="dropdown"><a className="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">EXP Level</a>
+                                    <div className="dropdown-menu" role="menu"><a className="dropdown-item" role="presentation" href="#">Beginner</a><a className="dropdown-item" role="presentation" href="#">Intermediate</a><a className="dropdown-item" role="presentation" href="#">Advanced</a></div>
                                 </li>
                             </ul>
-                            <form class="form-inline mr-auto" target="_self">
-                                <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="form-control search-field" type="search" name="search" id="search-field"></div>
+                            <form className="form-inline mr-auto" target="_self">
+                                <div className="form-group"><label for="search-field"><i className="fa fa-search"></i></label><input className="form-control search-field" type="search" name="search" id="search-field"/></div>
                             </form>
                     </div>
                 </nav>
               
-                        <div class="hero">
-                            <h1 class="text-center">We provide you the path</h1>
-                            <div class="hero__logo-flex"><img class="hero__logo" src="./frame.png" alt="logo"/></div>
+                        <div className="hero">
+                            <h1 className="text-center">We provide you the path</h1>
+                            <div className="hero__logo-flex"><img className="hero__logo" src="./frame.png" alt="logo"/></div>
                         </div>
                     
                 
