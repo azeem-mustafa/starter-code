5718451c46b7ba6c85b3604ed2119d7f0766ba3f
Azeem M <87535744+azeem-mustafa@users.noreply.github.com>
2021-09-06 16:20:01 -0400

    added footer
    finished

--- client/src/components/VideoCard/VideoCard.js
+++ client/src/components/VideoCard/VideoCard.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import './_videoCard.scss';
 
 const VideoCard = ({video}) => {
     console.log(video)
     return (
-        <div>
+        
+        <div className="video">
+
+
+        <p>Difficulty Level: {video.difficulty} </p>
+        <div className='video-card'>
+
+            <div className="video-card__img">
            <img src= {video.image} alt="" />
-           <p>Difficulty Level: {video.difficulty} </p> 
-           <div>
-               <p>{video.title}</p>
            </div>
+            
+           <div className='video-card__title'>
+               <p>THIS ISSS{video.title}</p>
+           </div>
+        </div>
         </div>
     );
 };
